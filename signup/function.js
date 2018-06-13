6aee3cca30028ad67dabf265ce7510f57d9a6381
Barry Low <barrylow@gmail.com>
2018-06-13 01:15:41 -0700

    Replace xhr with axios

--- signup/function.js
+++ signup/function.js
@@ -1,36 +1,35 @@
-var xhr = require("xhr")
+var axios = require('axios');
 
 module.exports = function(context, req) {
     context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
 
-    
-
     if (req.body && req.body.email) {
-        var data = JSON.stringify([
-        {
-            "email": req.body.email
-        }
-        ]);
+
         var API_KEY = process.env.SG_APIKEY;
 
-        xhr.withCredentials = true;
-        
-        xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === this.DONE) {
-            console.log(this.responseText);
-        }
+        axios.post({
+            method: 'post',
+            url: 'https://api.sendgrid.com/v3/contactdb/recipients',
+            data: {
+                email: req.body.email
+            },
+            headers:{
+                'content-type': 'application/json',
+                'authorization': 'Bearer' + API_KEY
+            }
+        })
+        .then(function (response) {
+            context.res = {
+                // status defaults to 200 */
+                body: "Success: Added " + req.body.email + response.persisted_recipients[0]
+            };
+        })
+        .catch(function (error) {
+            context.res = {
+                status: 400,
+                body: "Error: " + error
+            };
         });
-        
-        xhr.open("POST", "https://api.sendgrid.com/v3/contactdb/recipients");
-        xhr.setRequestHeader("authorization", "Bearer" + API_KEY);
-        xhr.setRequestHeader("content-type", "application/json");
-        
-        xhr.send(data);
-
-        context.res = {
-            // status defaults to 200 */
-            body: "Success: Added " + req.body.email
-        };
     }
     else {
         context.res = {
