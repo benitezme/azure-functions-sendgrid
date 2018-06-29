07bd975c694262d5e556221cc63cc734294625a1
Barry Low <barrylow@gmail.com>
2018-06-29 01:19:14 -0700

    Add recaptcha to sendcontact

--- sendcontact/index.js
+++ sendcontact/index.js
@@ -8,18 +8,21 @@ module.exports = function(context, req) {
     var name = '';
     var email = '';
     var message = '';
+    var recaptcha = '';
     var dev = false;
 
     if(req.method === 'GET'){
       name = req.params.name;
       email = req.params.email;
       message = req.params.message;
+      recaptcha = req.params.recaptcha;
       dev = req.params.dev;
     }
     if(req.method === 'POST'){
       name = req.body.name;
       email = req.body.email;
       message = req.body.message;
+      recaptcha = req.body.recaptcha;
       dev = req.body.dev;
     }
 
@@ -46,58 +49,87 @@ module.exports = function(context, req) {
                 ],
                 "subject": "AA Corporate Site Contact - Message from " + name,
                 "substitutions": {
+                  "-aacontactname-": name,
+                  "-aacontactemail-": email,
                   "-aacontactbody-": message
                 }
               }
             ],
             "from": {
-              "email": email
+              "email": email,
               "name": name
             },
             "reply_to": {
-              "email": email
+              "email": email,
               "name": name
             },
             "template_id": process.env.TEMPL_ID
           });
 
-        var sendVerify = axios({
-            method: 'post',
-            url: 'https://api.sendgrid.com/v3/mail/send',
-            data: data,
-            headers:{
-                'content-type': 'application/json',
-                'authorization': 'Bearer ' + API_KEY
-            }
-        })
-        .then(function (response) {
-            if (response.status >= 200 && response.status < 300) {
+          var checkCaptcha = axios({
+              async: false,
+              method: 'post',
+              url: 'https://www.google.com/recaptcha/api/siteverify',
+              data: {'secret': process.env.RECAPTCHA, 'response': },
+              headers:{
+                  'content-type': 'application/json',
+                  'authorization': 'Bearer ' + API_KEY
+              }
+          })
+          .then(function (response) {
+              if (response.status >= 200 && response.status < 300) {
+                  return true;
+              }
+          })
+          .catch(function (error) {
+              context.res = {
+                  status: 400,
+                  body: "reCaptcha error: " + error.response.data.errors[0].message,
+                  headers
+              };
+              context.done();
+              return false;
+          });
+
+          if(checkCaptcha){
+            var sendVerify = axios({
+                method: 'post',
+                url: 'https://api.sendgrid.com/v3/mail/send',
+                data: data,
+                headers:{
+                    'content-type': 'application/json',
+                    'authorization': 'Bearer ' + API_KEY
+                }
+            })
+            .then(function (response) {
+                if (response.status >= 200 && response.status < 300) {
+                    context.res = {
+                        status: response.status,
+                        body: "Contact email sent \n",
+                        headers
+                    };
+                    return context;
+                } else {
+                    throw response.data.errors[0].message;
+                }
+            })
+            .catch(function (error) {
                 context.res = {
-                    status: response.status,
-                    body: "Contact email sent \n",
+                    status: 400,
+                    body: "Contact email send error: " + error.response.data.errors[0].message,
                     headers
                 };
-                return context;
-            } else {
-                throw response.data.errors[0].message;
-            }
-        })
-        .catch(function (error) {
+                return error.status;
+            });
+            return sendVerify;
+        }
+        else {
             context.res = {
                 status: 400,
-                body: "Contact email send error: " + error.response.data.errors[0].message,
+                body: "Please pass an email address in the request body" + context.res,
                 headers
             };
-            return error.status;
-        });
-        return sendVerify;
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass an email address in the request body" + context.res,
-            headers
-        };
-    }
+        }
+      }
     context.done();
 };
